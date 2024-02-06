cb446b2ab07353ddcc177e285c6b8fd515d43e1f
Alan Patric Rodrigues <patric0889@gmail.com>
2024-02-06 19:05:07 -0300

    Changing elements

---
+++ start.bat
@@ -0,0 +1,21 @@
+CALL "C:\Users\patri\anaconda3\condabin\conda.bat" activate PrivateGPT2
+cd c:\privateGPT2
+
+START Make run 
+CALL "Make run" 
+CMD "Make run"
+
+CMD "C:\Users\patri\anaconda3\condabin\conda.bat" activate PrivateGPT2
+
+START Make run 
+CALL "Make run" 
+CMD "Make run"
+ 
+::CMD /c "Make run"::
+::Make run
+::Start "Make run"
+::RUN "Make run"
+::Call Make run
+
+
+
